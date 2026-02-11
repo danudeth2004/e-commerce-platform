95b26f37635a2c16402b5303a2af698c8dcf1a73
danudeth2004 <oat25482547@gmail.com>
2026-02-11 18:56:24 +0700

    feat: add avatar user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
   validates :first_name, :last_name, presence: true
   validates :phone_number, format: { with: /\A0\d{9}\z/ }
 end
