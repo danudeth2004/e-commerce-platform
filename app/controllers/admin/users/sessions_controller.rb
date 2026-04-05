afdecda183903c75e47b32f3ed0b535b9c221741
danudeth2004 <oat25482547@gmail.com>
2026-04-05 14:34:55 +0700

    style: admin page layout

--- app/controllers/admin/users/sessions_controller.rb
+++ app/controllers/admin/users/sessions_controller.rb
@@ -32,4 +32,8 @@ class Admin::Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(_resource)
     admin_root_path
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_user_session_path
+  end
 end
