6910dc94c97080c4342e985e2574c372e9bd3b50
danudeth2004 <oat25482547@gmail.com>
2026-04-06 18:08:23 +0700

    fix: enchance threads and pool for prod

--- config/puma.rb
+++ config/puma.rb
@@ -25,7 +25,7 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads 1, 2
+threads 1, 1
 preload_app!
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
